b7be331d43588a2c829b92707496cc19d4336cda
UtkarshDixit-py <utkarshdixit303@gmail.com>
2022-08-17 23:20:58 +0530

    event listeners

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -20,7 +20,13 @@ self.addEventListener('install',(event)=>{
 //Listen for reqs
 
 self.addEventListener('fetch',(event)=>{
-
+    event.respondWith(
+        caches.match(event.request)
+            .then(()=>{
+                return fetch(event.request)
+                    .catch(()=> caches.match('offline.html'))
+            })
+    )
 });
 
 //Activate the SW
